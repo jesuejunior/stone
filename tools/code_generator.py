a3aabc6bf36c057c2a01d10fcbdde02cbbf9ad84
Jesue Junior <jesuesousa@gmail.com>
2016-04-21 17:37:34 -0300

    ...

--- tools/code_generator.py
+++ tools/code_generator.py
@@ -2,8 +2,7 @@
 
 import string
 import random
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
-for x in range(0, 10):
-    print(id_generator())
+
+def generate_code(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
