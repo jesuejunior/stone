85ebdf41ff35a509f865b731cb05ad43bff11937
Jesue Junior <jesuesousa@gmail.com>
2016-04-21 16:04:00 -0300

    Addin onix app

--- onix/forms.py
+++ onix/forms.py
@@ -1,13 +1,16 @@
 # encoding: utf-8
-from django.forms import TextInput, ModelForm, ChoiceField
+from django.forms import TextInput, ModelForm, TextField
 
-from onix.models import Question
+from onix.models import Block
 
 
-class QuestionForm(ModelForm):
-    answer = ChoiceField(widget=TextInput(attrs={
-        'placeholder': 'Marque a resposta correta.',
+class BlockForm(ModelForm):
+    name = TextField(widget=TextInput(attrs={
+        'placeholder': 'Digite o nome do material.',
+    }))
+    number = TextField(widget=TextInput(attrs={
+        'placeholder': 'Digite o numero do bloco.',
     }))
 
     class Meta:
-        model = Question
+        model = Block
--- onix/views.py
+++ onix/views.py
@@ -1,26 +1,31 @@
 # Create your views here.
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, InvalidPage
 from django.core.urlresolvers import reverse
-from django.db.models import F, Count
 from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
 from django.views.generic import View, ListView
 from braces.views import LoginRequiredMixin
-from onix.models import Question, Unity, Result
+from onix.models import Block
+from tools.code_generator import generate_code
 
 
-class HomeTemplateView(LoginRequiredMixin, ListView):
+class HomeTemplateView(LoginRequiredMixin, View):
     template_name = 'home.html'
-    model = Unity
-
-
-class QuestionTemplateView(LoginRequiredMixin, View):
-    template_name = 'question.html'
 
     def get(self, request, *args, **kwargs):
-        lesson_id = args[0]
-        question_id = args[1]
-        cxt = self.get_context_data(request, lesson_id, question_id)
+        return TemplateResponse(
+            request=self.request,
+            template=self.template_name,
+            context={},
+            **kwargs
+        )
+
+
+class BlockTemplateView(LoginRequiredMixin, View):
+    template_name = 'block/new.html'
+
+    def get(self, request, *args, **kwargs):
+        cxt = self.get_context_data(request)
         return TemplateResponse(
             request=self.request,
             template=self.template_name,
@@ -29,61 +34,60 @@ class QuestionTemplateView(LoginRequiredMixin, View):
         )
 
     def post(self, request, *args, **kwargs):
-        lesson_id = args[0]
-        question_id = args[1]
-        self.get_context_data(request, lesson_id, question_id)
-        req = request.POST
-        answer = req.get('answer')
-        if request.user.is_authenticated() and answer:
-            result = Result.objects.get_or_create(user=request.user, lesson_id=lesson_id)
-            if self.question.answer_correct.id == int(answer[0]):
-                result[0].correct = F('correct') + 1
-            else:
-                result[0].wrong = F('wrong') + 1
-            result[0].save()
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+        code = None
 
-        if self.finish:
-            return HttpResponseRedirect(reverse('results', kwargs={'lesson_id': lesson_id}))
+        while(True):
+            code = generate_code()
+            exist = Block.objects.filter(code=code).exists()
+            if not exist:
+                break
 
-        return HttpResponseRedirect(reverse('question', args=(lesson_id, int(question_id)+1)))
+        if request.user.is_authenticated() and number:
+            result = Block.objects.get_or_create(name=name, number=number, code=code)
 
-    def get_context_data(self, request, lesson_id, question_id):
+            saved = result[0].save()
 
-        _question = Question.objects.prefetch_related().filter(lesson__id=lesson_id)[:10]
-        paginator = Paginator(_question, 1)
+        if saved:
+            return HttpResponseRedirect(reverse('blocks'))
+
+        return HttpResponseRedirect(reverse('blocks'))
+
+    def get_context_data(self, request):
+
+        _question = Block.objects.all()
+        paginator = Paginator(_question, 50)
+
+        page = request.GET.get('page')
 
         try:
-            page = paginator.page(question_id)
+            blocks = paginator.page(page)
         except PageNotAnInteger:
-            page = paginator.page(1)
+            blocks = paginator.page(1)
         except (EmptyPage, InvalidPage):
-            page = paginator.page(paginator.num_pages)
+            blocks = paginator.page(paginator.num_pages)
 
-        self.question = page.object_list[0]
-        self.finish = not page.has_next()
         cxt = {
-            'question': self.question,
-            'answers': self.question.answers.all,
+            'blocks': blocks,
         }
 
         return cxt
 
 
-class ResultTemplateView(LoginRequiredMixin, View):
-    template_name = 'results.html'
-    model = Result
+class BlockListTemplateView(LoginRequiredMixin, ListView):
+    template_name = 'block/list.html'
+    model = Block
 
-    def get(self, request, *args, **kwargs):
-        lesson_id = kwargs.pop('lesson_id')
-        cxt = self.get_context_data(request, lesson_id)
-        return TemplateResponse(
-            request=self.request,
-            template=self.template_name,
-            context=cxt,
-            **kwargs
-        )
-
-    def get_context_data(self, request, lesson_id):
-        user = request.user
-        result = self.model.objects.filter(user_id=user.id, lesson_id=lesson_id).latest('finished_at').annotate(correct=Count('correct'))
-        return {'results': [result], 'lesson': result.lesson}
+    # def get(self, request, *args, **kwargs):
+    #     cxt = self.get_context_data(request)
+    #     return TemplateResponse(
+    #         request=self.request,
+    #         template=self.template_name,
+    #         context=cxt,
+    #         **kwargs
+    #     )
+    #
+    # def get_context_data(self, request):
+    #     result = self.model.objects.all()
+    #     return {'results': result}
